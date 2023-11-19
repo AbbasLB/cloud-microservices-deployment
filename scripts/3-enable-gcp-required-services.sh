b8fe81fe7bc8259193d055670d02ca100cf4cadc
Abbas Khreiss <AbbasKhreiss0@gmail.com>
2023-11-20 00:38:10 +0100

    Edited required services script

--- scripts/3-enable-gcp-required-services.sh
+++ scripts/3-enable-gcp-required-services.sh
@@ -3,3 +3,4 @@ gcloud services enable container.googleapis.com
 gcloud services enable monitoring.googleapis.com
 gcloud services enable cloudtrace.googleapis.com
 gcloud services enable cloudprofiler.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com
