273217a6d01e9333eb959916b754d555ac8b4008
Abbas Khreiss <AbbasKhreiss0@gmail.com>
2023-11-20 00:32:25 +0100

    Added new script to enable gcp required services

---
+++ scripts/3-enable-gcp-required-services.sh
@@ -0,0 +1,5 @@
+gcloud services enable compute.googleapis.com
+gcloud services enable container.googleapis.com
+gcloud services enable monitoring.googleapis.com
+gcloud services enable cloudtrace.googleapis.com
+gcloud services enable cloudprofiler.googleapis.com
