ce3232edb9fedf72b05e868fa5f5e993e365cbfd
Abbas Khreiss <AbbasKhreiss0@gmail.com>
2023-11-26 18:08:15 +0100

    Ediit load gen deployment script to get ip automatically

--- scripts/5-boutique-deploy-load-generator.sh
+++ scripts/5-boutique-deploy-load-generator.sh
@@ -1,3 +1,10 @@
 cd "$(dirname "$(realpath "$0")")"/../load-generator-deployment
 terraform init
-terraform apply
+
+
+# Run the script and capture the output
+frontend_addr=$(bash 4-boutique-get-ip-address.sh | tail -n 1)
+
+# Apply the value to Terraform using the -var flag
+terraform apply -var="frontend_addr=${frontend_addr}"
+
