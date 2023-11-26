6f1320f2200fab9addfcca732ffbc7692cf96428
Abbas Khreiss <AbbasKhreiss0@gmail.com>
2023-11-26 16:13:36 +0100

    minor change to scripts

--- scripts/6-deploy-monitoring.sh
+++ scripts/6-deploy-monitoring.sh
@@ -6,6 +6,8 @@ kubectl apply -f monitoring-deployment/prometheus
 kubectl apply -f monitoring-deployment/node-exporter
 kubectl apply -f monitoring-deployment/grafana
 
+echo "Waiting for prometheus and grafana pods to be ready ..."
+
 kubectl wait --for=condition=ready pods -l app=prometheus-server -n monitoring --timeout=300s
 kubectl wait --for=condition=ready pods -l app=grafana -n monitoring --timeout=300s
 
