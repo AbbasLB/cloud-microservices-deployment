d106a8d3b5fde35e32ad72a9a8502f8143f74c9f
Abbas Khreiss <AbbasKhreiss0@gmail.com>
2023-11-26 15:30:04 +0100

    Add wait for monitoring deployment

--- scripts/6-deploy-monitoring.sh
+++ scripts/6-deploy-monitoring.sh
@@ -6,4 +6,6 @@ kubectl apply -f monitoring-deployment/prometheus
 kubectl apply -f monitoring-deployment/node-exporter
 kubectl apply -f monitoring-deployment/grafana
 
+kubectl wait --for=condition=ready pods --all -n monitoring --timeout=300s
+
 
