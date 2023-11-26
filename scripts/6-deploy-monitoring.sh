abde4885ea24d4d55776917d53a4c61e133f62db
Abbas Khreiss <AbbasKhreiss0@gmail.com>
2023-11-26 15:56:18 +0100

    give admin role to prometheus

--- scripts/6-deploy-monitoring.sh
+++ scripts/6-deploy-monitoring.sh
@@ -6,6 +6,8 @@ kubectl apply -f monitoring-deployment/prometheus
 kubectl apply -f monitoring-deployment/node-exporter
 kubectl apply -f monitoring-deployment/grafana
 
-kubectl wait --for=condition=ready pods --all -n monitoring --timeout=300s
+kubectl wait --for=condition=ready pods -l app=prometheus-server -n monitoring --timeout=300s
+kubectl wait --for=condition=ready pods -l app=grafana -n monitoring --timeout=300s
+
 
 
