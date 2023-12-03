5da87528a07cf0c5f0438b2b91ccef4e077aa60c
Abbas Khreiss <abbaskhreiss0@gmail.com>
2023-12-03 19:09:02 +0000

    Add canary scripts

---
+++ scripts/7-setup-canary.sh
@@ -0,0 +1,8 @@
+
+cd "$(dirname "$(realpath "$0")")"/../boutique-deployment/terraform
+gcp_project_id=$(grep 'gcp_project_id' terraform.tfvars | sed 's/.*= *//; s/"//g')
+
+cd "$(dirname "$(realpath "$0")")"/../canary/gateway
+
+./setup-gateway-and-istio.sh
+./create-delivery-pipeline.sh $gcp_project_id
---
+++ scripts/9-release-canary-frontend-v3.sh
@@ -0,0 +1,9 @@
+
+cd "$(dirname "$(realpath "$0")")"/../boutique-deployment/terraform
+gcp_project_id=$(grep 'gcp_project_id' terraform.tfvars | sed 's/.*= *//; s/"//g')
+
+cd "$(dirname "$(realpath "$0")")"/../canary/gateway
+
+./create-release.sh frontend-release-v3 $gcp_project_id gcr.io/cloud-computing-lab-1-402606/frontend:v3
+
+echo "Visit https://console.cloud.google.com/deploy/delivery-pipelines to advance the rollout"
